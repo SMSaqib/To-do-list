b1b879a99e4e7392885703cc935b7675ee852f92
Shaikh Muhammad Saqib <saqib.kareem.gmb@gmail.com>
2023-09-14 16:45:34 +0500

    updated

---
+++ app.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import { task_list, confirmation, removal, cont, activity_re, activity, Removal_confirm_afteraddition } from "./functions.js";
+console.log("To-do App | Plan your Day to Day Activity");
+let removalconfirmationafteraddition;
+let continiuty;
+let d = await confirmation();
+console.log(d.conf);
+switch (d.conf) {
+    case "Please Add your Activity":
+        do {
+            let answer = await inquirer.prompt([
+                {
+                    message: "Please Enter your activity",
+                    type: "string",
+                    name: "act",
+                },
+            ]);
+            activity(answer.act);
+            console.log(task_list);
+            continiuty = await cont();
+            console.log(task_list);
+        } while (continiuty == "Yes");
+        if (continiuty == "No") {
+            // return {remov_conf: Removal_confirm_afteraddition.remov_conf}
+            do {
+                removalconfirmationafteraddition = await Removal_confirm_afteraddition();
+                if (removalconfirmationafteraddition == "Yes") {
+                    let x = await removal();
+                    activity_re(x.indNo, x.Rec_No);
+                    //   result = num1 - num2;
+                    console.log(task_list);
+                    console.log("you have suucessfully removed the task from the list");
+                    //  continiuty= await cont()
+                    console.log(task_list);
+                }
+                else
+                    break;
+            } while (removalconfirmationafteraddition == "Yes");
+        }
+        //    result = num1 + num2;
+        break;
+    //   case "Do you want to remove Activity?":
+    //   do {
+    //   let x =await  removal()  
+    //   activity_re(x.indNo,x.Rec_No)
+    //  //   result = num1 - num2;
+    //  console.log(task_list)
+    //  console.log("you have suucessfully removed the task from the list") 
+    //  continiuty= await cont()
+    //    console.log(task_list)
+    //   } while(continiuty =="Yes")
+    //   break;
+    default:
+        console.log(task_list);
+        console.log("you have successfuly log Out");
+}
+//console.log(`Result: ${num1} ${operator} ${num2} = ${result}`);
---
+++ functions.js
@@ -0,0 +1,64 @@
+import inquirer from "inquirer";
+let task_list = [];
+let continiuty;
+////////////////////////// step-01 cuser feedback
+async function confirmation() {
+    let confirmation = await inquirer.prompt([
+        {
+            message: "Please select your preferrence",
+            type: "list",
+            name: "conf",
+            choices: ["Please Add your Activity", "Exit"]
+        },
+    ]);
+    return { conf: confirmation.conf };
+}
+////////////////////////// step-02 push data
+function activity(task) {
+    task_list.push(task);
+    return task_list;
+}
+////////////////////////// step-03 remove data
+function activity_re(index, record) {
+    task_list.splice(index, record);
+    return task_list;
+}
+////////////////////////// step-03 continue data
+async function cont() {
+    let continuation = await inquirer.prompt([
+        {
+            message: "Do you want to Continue? ",
+            type: "list",
+            name: "conf",
+            choices: ["Yes", "No"]
+        },
+    ]);
+    return continuation.conf;
+}
+async function removal() {
+    let rem = await inquirer.prompt([
+        {
+            message: "Enter Index Number",
+            type: "input",
+            name: "indNo",
+        },
+        {
+            message: "Enter Record ",
+            type: "input",
+            name: "Rec_No",
+        },
+    ]);
+    return rem;
+}
+async function Removal_confirm_afteraddition() {
+    let x = await inquirer.prompt([{
+            message: "Do you want to remove?",
+            type: "list",
+            choices: ["Yes", "No"],
+            name: "remov_conf"
+        }]);
+    return x.remov_conf;
+}
+// let x=await Removal_confirm_afteraddition()
+// console.log(x)
+export { task_list, continiuty, confirmation, removal, cont, activity_re, activity, Removal_confirm_afteraddition };
